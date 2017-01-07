68e67f2e45d96ee93095539a3891f7c2c1b9c44c
Amsify42 <amsify42@gmail.com>
2017-01-08 01:03:31 +0530

    Update Pagination.php

--- core/Pagination.php
+++ core/Pagination.php
@@ -1,74 +1,127 @@
 <?php
+/**
+ *  This class is extending CI_Model and being extended by MY_Model.
+ *  All the pagination functionalities are in this class
+ *  _call which will filter results based on settings of model class
+ */
 class Pagination extends CI_Model {
 
-	private    $page           = 1;
-	private    $config         = array();
-	private    $conditions     = false;
-	private    $limit  	       = 1; 
-	private    $customQuery;
-  	protected  $CI_Conditions  = array();
-  	protected  $relations      = array();
-  	protected  $objectRelease  = array(
-					'get',
-					'result',
-					'result_array',
-					'result_object',
-					'custom_result_object',
-					'row',
-					'unbuffered_row',
-					'row_array',
-					'row_object',
-					'custom_row_object',
-					'data_seek',
-					'set_row',
-					'next_row',
-					'previous_row',
-					'first_row',
-					'last_row',
-					'num_rows',
-					'num_fields',
-					'field_data',
-					'free_result',
-					'list_fields'
-					); 		
+  /**
+   * Pagination page number
+   * @var integer
+   */
+  private    $page           = 1;
+
+  /**
+   * This is the array of pagination config
+   * @var array
+   */
+  private    $config         = array();
+
+  /**
+   * Set true if conditions passed for pagination
+   * @var boolean
+   */
+  private    $conditions     = false;
+
+  /**
+   * Pagination limit per page
+   * @var integer
+   */
+  private    $limit  	     = 1; 
+
+  /**
+   * Query created based on conditions passed
+   * @var string
+   */
+  private    $customQuery;
+
+  /**
+   * This is a collection of conditions passed to pagination
+   * @var array
+   */
+  protected  $CI_Conditions  = array();
+
+  /**
+   * This is a collection of relations added in model class
+   * @var array
+   */
+  protected  $relations      = array();
+
+  /**
+   * This is a collection of CI db method names which release results
+   * @var array
+   */
+  protected  $objectRelease  = array(
+                                'get',
+                                'result',
+                                'result_array',
+                                'result_object',
+                                'custom_result_object',
+                                'row',
+                                'unbuffered_row',
+                                'row_array',
+                                'row_object',
+                                'custom_row_object',
+                                'data_seek',
+                                'set_row',
+                                'next_row',
+                                'previous_row',
+                                'first_row',
+                                'last_row',
+                                'num_rows',
+                                'num_fields',
+                                'field_data',
+                                'free_result',
+                                'list_fields'
+                                ); 		
 
 
- 	function __construct(){
-	      parent::__construct();
-	      $this->config['uri_segment']        = $this->getURISegment();
-	      $this->config['base_url']           = $this->getCurrentURL();
-	      $this->config['num_links']          = 5; 
-	      $this->config['use_page_numbers']   = TRUE;
-	      $this->config['page_query_string']  = FALSE;
+  /**
+   * __construct contains the default config of pagination
+   */
+  function __construct(){
+      parent::__construct();
+      $this->config['uri_segment']        = $this->getURISegment();
+      $this->config['base_url']           = $this->getCurrentURL();
+      $this->config['num_links']          = 5; 
+      $this->config['use_page_numbers']   = TRUE;
+      $this->config['page_query_string']  = FALSE;
 
-	      $this->config['full_tag_open']      = '<ul class="pagination">';
-	      $this->config['full_tag_close']     = '</ul>';
+      $this->config['full_tag_open']      = '<ul class="pagination">';
+      $this->config['full_tag_close']     = '</ul>';
 
-	      $this->config['first_link']         = 'First';
-	      $this->config['first_tag_open']     = '<li>';
-	      $this->config['first_tag_close']    = '</li>';
+      $this->config['first_link']         = 'First';
+      $this->config['first_tag_open']     = '<li>';
+      $this->config['first_tag_close']    = '</li>';
 
-	      $this->config['prev_link']          = 'Prev';
-	      $this->config['prev_tag_open']      = '<li>';
-	      $this->config['prev_tag_close']     = '</li>';
+      $this->config['prev_link']          = 'Prev';
+      $this->config['prev_tag_open']      = '<li>';
+      $this->config['prev_tag_close']     = '</li>';
 
-	      $this->config['next_link']          = 'Next';
-	      $this->config['next_tag_open']      = '<li>';
-	      $this->config['next_tag_close']     = '</li>';
+      $this->config['next_link']          = 'Next';
+      $this->config['next_tag_open']      = '<li>';
+      $this->config['next_tag_close']     = '</li>';
 
-	      $this->config['cur_tag_open']       = '<li class="active"><a href="#">';
-	      $this->config['cur_tag_close']      = '</a></li>';
+      $this->config['cur_tag_open']       = '<li class="active"><a href="#">';
+      $this->config['cur_tag_close']      = '</a></li>';
 
-	      $this->config['num_tag_open']       = '<li>';
-	      $this->config['num_tag_close']      = '</li>';
+      $this->config['num_tag_open']       = '<li>';
+      $this->config['num_tag_close']      = '</li>';
 
-	      $this->config['last_link']          = 'Last';
-	      $this->config['last_tag_open']      = '<li>';
-	      $this->config['last_tag_close']     = '</li>';
+      $this->config['last_link']          = 'Last';
+      $this->config['last_tag_open']      = '<li>';
+      $this->config['last_tag_close']     = '</li>';
 
     }	
 
 
+    /**
+     * __call will filer results based on settings passed in model class
+     * @param  string $method
+     * @param  array $args
+     * @return array
+     */
     public function __call($method, $args){
        if(method_exists($this->db, $method) && is_callable(array($this->db, $method))) {
           if(in_array($method, $this->objectRelease)) {
@@ -76,7 +129,7 @@ class Pagination extends CI_Model {
               if($method == 'get') {
                 return call_user_func_array(array($this->db, $method), $args);
               } else {
-                return $this->filterRelations(call_user_func_array(array($this->db, $method), $args));
+                return $this->filterResults(call_user_func_array(array($this->db, $method), $args));
               }
           } else {
             $this->CI_Conditions[] = array('method' => $method, 'args' => $args);
@@ -86,12 +139,17 @@ class Pagination extends CI_Model {
         }
         else if(in_array($method, $this->objectRelease)) {
             $this->db->from($this->table);
-            return $this->filterRelations(call_user_func_array(array($this->db->get(), $method), $args));
+            return $this->filterResults(call_user_func_array(array($this->db->get(), $method), $args));
         }
     }
 
 
-
+    /**
+     * Set conditions for pagination
+     * @param  object  $conditions
+     * @param  boolean $query
+     * @return object
+     */
     public function conditions($conditions, $query = false) {
         if($query) {
           return $conditions($query);
@@ -105,7 +163,13 @@ class Pagination extends CI_Model {
 
 
 
-
+    /**
+     * Get paginated result
+     * @param  string $limit
+     * @param  string $page
+     * @param  array  $config
+     * @return array
+     */
     public function paginate($limit = '', $page = '', $config = array()) {
         // Update Limit If supplied
         if($limit != '') {
@@ -114,7 +178,7 @@ class Pagination extends CI_Model {
 
         // Update Config If supplied
         if(sizeof($config) > 0) {
-          $this->config   = $this->exchangeUpdatedKeys($config);
+          $this->config   = $this->paginationConfig($config);
         }
 
         if($page != '') {
@@ -129,9 +193,14 @@ class Pagination extends CI_Model {
     }
 
 
+    /**
+     * Get paginated result
+     * @param  integer $limit
+     * @param  integer $page
+     * @return array
+     */
     public function getPaginateResult($limit, $page) {
 
-        
         $this->db->from($this->table);
 
         if($page == 1) {
@@ -151,11 +220,16 @@ class Pagination extends CI_Model {
         if(!$query->num_rows()){
           return $this->data;
         } else {
-          return $this->filterRelations($query->result());
+          return $this->filterResults($query->result());
         }
-
     }
 
+
+
+    /**
+     * Count rows of result
+     * @return integer
+     */
     public function countRows() {
         $this->db->trans_start();
         $this->db->select('id');
@@ -173,6 +247,10 @@ class Pagination extends CI_Model {
     }
 
 
+    /**
+     * Get uri segment
+     * @return integer
+     */
     public function getURISegment() {
       $last     = $this->uri->total_segments();
       $segment  = $this->uri->segment($last);
@@ -184,6 +262,10 @@ class Pagination extends CI_Model {
     } 
 
 
+    /**
+     * Get current url
+     * @return string
+     */
     public function getCurrentURL() {
        $currentURL = base_url(uri_string());  
        $segment    = $this->uri->segment($this->config['uri_segment']);
@@ -195,16 +277,29 @@ class Pagination extends CI_Model {
     }
 
 
-
+    /**
+     * Generate Pagination links
+     * @return string
+     */
     public function getLinks() {        
         return $this->paginationLinks();
     }
 
+
+    /**
+     * Get total rows of result
+     * @return integer
+     */
     public function getTotalRows() {
 		return $this->config['total_rows'];
     }
 
 
+
+    /**
+     * Generate Pagination links
+     * @return string
+     */
     public function paginationLinks() {
          
         $this->config['per_page']     = $this->limit;
@@ -216,7 +311,12 @@ class Pagination extends CI_Model {
     }
 
 
-    public function exchangeUpdatedKeys($config) {
+    /**
+     * For setting config for pagination
+     * @param  array $config
+     * @return array
+     */
+    public function paginationConfig($config) {
       if(sizeof($config) > 0) {
         foreach ($config as $key => $value) {
             $this->config[$key] = $value;
@@ -224,6 +324,5 @@ class Pagination extends CI_Model {
       }
       return $this->config;
     }
-	
 
 }
