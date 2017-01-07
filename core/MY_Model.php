ecf0c730587d868c0a6b7c1d8dfb36d40bb35e53
Amsify42 <amsify42@gmail.com>
2017-01-08 00:59:36 +0530

    Update MY_Model.php

--- core/MY_Model.php
+++ core/MY_Model.php
@@ -1,13 +1,56 @@
 <?php
 require_once 'Pagination.php';
+
+/**
+ *  This class must be extended by model classes to inherit the functionalities
+ */
 class MY_Model extends Pagination {
 
-   protected   $table        = '';
-   protected   $data         = array();	
+   /**
+    * Name of the database table
+    * @var string
+    */
+	 protected   $table        = '';
+
+   /**
+    * Database table column names
+    * @var array
+    */
+   protected   $fillable     = array();
+
+   /**
+    * Database table column names
+    * @var array
+    */
+   protected   $hidden       = array();
+
+   /**
+    * Temporary array for returning
+    * @var array
+    */
+	 protected   $data         = array();	
+
+   /**
+    * True, if relational data is being called
+    * @var boolean
+    */
    protected   $getRelation  = false;
+
+   /**
+    * For checking result type fetched by queries
+    * @var string
+    */
    protected   $resultType   = 'object';
+
+   /**
+    * For checking result fetched is single row or multiple
+    * @var string
+    */
    protected   $rowType      = 'multiple';
 
+   /**
+    * This will set automatic table name based on name of model class
+    */
    function __construct(){
       parent::__construct();
       if($this->table == '') {
@@ -15,10 +58,20 @@ class MY_Model extends Pagination {
       }
     }
 
+    /**
+     * For getting the name of table
+     * @return string
+     */
     public function getTable() {
       return $this->table;
     }
 
+    /**
+     * It will simply send the result of raw query passed
+     * @param  string $query
+     * @param  string $type
+     * @return array
+     */
     public function rawQuery($query, $type = 'object') {
       if($type == 'array') {
         return $this->db->query($query)->result_array();  
@@ -26,14 +79,29 @@ class MY_Model extends Pagination {
       return $this->db->query($query)->result();
     }
 
+    /**
+     * count of all rows present in table 
+     * @return integer
+     */
     public function count_all() {
       return $this->db->count_all($this->table);
     }
 
+
+    /**
+     * get all rows present in table 
+     * @return array
+     */
     public function getAll() {
       return $this->db->select('*')->from($this->table)->get()->result();
     }
 
+
+    /**
+     * Get rows based on array of conditions passed
+     * @param  array $conditions
+     * @return aray or boolean
+     */
     public function getWhere($conditions) {
       if(is_array($conditions)) {
         return $this->db->get_where($this->table, $conditions)->result();
@@ -41,7 +109,13 @@ class MY_Model extends Pagination {
       return false;
     }
 
-
+    /**
+     * Get rows based on multiple Ids passed with column name
+     * @param  array  $IDsArray
+     * @param  string $column
+     * @param  string $type
+     * @return array
+     */
     public function getWhereIDs($IDsArray, $column = 'id', $type = 'object') {
       if($IDsArray) {
         if(is_array($IDsArray)) {
@@ -56,6 +130,12 @@ class MY_Model extends Pagination {
       return $this->data;
     }
 
+
+    /**
+     * Get single row based on id or condition is passed
+     * @param  integer $id
+     * @return array
+     */
     public function find($id) {
     	if($id) {
         if(is_array($id)) {
@@ -67,59 +147,113 @@ class MY_Model extends Pagination {
     	return $this->data;
     }
 
+
+    /**
+     * For inserting single row in table
+     * @param  array  $data [description]
+     * @return integer or boolean
+     */
     public function insert($data = array()) {
      	if(sizeof($data) > 0) {
+        $data = $this->filterFillable($data);
     		$this->db->insert($this->table, $data);
         return $this->db->insert_id();
     	}
       	return false;
     }
 
+
+    /**
+     * For inserting multiple rows in table
+     * @param  array  $data [description]
+     * @return boolean
+     */
     public function insertBatch($data = array()) {
       if(sizeof($data) > 0) {
+        $data = $this->filterFillable($data, 'batch');
         return $this->db->insert_batch($this->table, $data);
       }
         return false;
     }
 
+
+    /**
+     * For updating a row in table by id or condition
+     * @param  integer or array $id
+     * @param  array  $data
+     * @return boolean
+     */
     public function update($id, $data = array()) {
      	if($id && sizeof($data) > 0) {
-     		return $this->db->where('id', $id)->update($this->table, $data);
+        $data = $this->filterFillable($data);
+        if(is_array($id)) {
+     		 return $this->db->where($id)->update($this->table, $data);
+        } else {
+          return $this->db->where('id', $id)->update($this->table, $data);
+        }
       }
       	return false;
     }
 
+    /**
+     * For deleting single row by id or conditions
+     * @param  integer or array $id
+     * @return boolean
+     */
     public function delete($id) {
     	if($id) {
-       	return $this->db->where('id', $id)->delete($this->table);
+        if(is_array($id)) {
+          return $this->db->where($id)->delete($this->table);
+        } else {
+          return $this->db->where('id', $id)->delete($this->table);
+        }
+      }
+      return false;
+    }
+
+    /**
+     * For deleting multiple rows by conditions
+     * @param  array  $conditions
+     * @param  string $column
+     * @return boolean
+     */
+    public function deleteBatch($conditions, $column = 'id') {
+      if($conditions) {
+        if(is_array($conditions)) {
+          $IDsArray = $this->getIDsArray($conditions, $id);
+          $this->db->where_in($column, $IDsArray);
+          return $this->db->delete($this->table);
+        } 
       }
       return false;
     }
 
 
-    public function deleteBatch($conditions) {
-      if($conditions) {
-        if(is_array($conditions)) {
-          $IDsArray = $this->getIDsArray($conditions);
-          $this->db->where_in('id', $IDsArray);
-          $this->db->delete($this->table);
-        } 
-      }
-      return $this->data;
-    }
-
-    public function getIDsArray($conditions) {
-      $result = $this->db->select('id')->get_where($this->table, $conditions)->result_array();
+    /**
+     * For getting all the Ids of array of result
+     * @param  array  $conditions
+     * @param  string $id
+     * @return array
+     */
+    public function getIDsArray($conditions, $id = 'id') {
+      $result = $this->db->select($id)->get_where($this->table, $conditions)->result_array();
       $IDs    = array();
       if(sizeof($result)> 0) {
         foreach ($result as $res) {
-          $IDs[] = $res['id'];
+          $IDs[] = $res[$id];
         }
       }
       return $IDs;
     }
 
 
+    /**
+     * Check if row existed with column value
+     * @param  string  $column
+     * @param  any     $value
+     * @param  integer $id
+     * @return boolean
+     */
     public function checkUnique($column, $value, $id = 0) {
         $row = array();
         if($id == 0) {
@@ -137,13 +271,50 @@ class MY_Model extends Pagination {
 
 
 
-   /* Relational Methods  */
+   /**
+    * For converting data set into fillable column names
+    * @param  array   $data
+    * @param  boolean $batch
+    * @return array
+    */
+   private function filterFillable($data, $batch = false) {
+      if(sizeof($this->fillable) > 0) {
+        if($batch) {
+          foreach($data as $key => $row) {
+            foreach($row as $col => $val) {
+              if(!in_array($col, $this->fillable)) {
+                unset($data[$key][$col]);
+              }
+            }
+          }
+        } else {
+          foreach($data as $col => $val) {
+            if(!in_array($col, $this->fillable)) {
+              unset($data[$col]);
+            }
+          }
+        }
+      }
+      return $data;
+   }
 
+
+   /**
+    * For setting relations as true for fetching relational result
+    * @return object
+    */
    public function relate() {
       $this->getRelation = true;
       return $this;
     }
 
+   /**
+    * For getting all the Ids of the result
+    * @param  array  $result
+    * @param  string $id
+    * @param  string $column
+    * @return array
+    */
    public function getResultIDsArray($result, $id = 'id', $column = '') {
 
       $IDs    = array();
@@ -185,12 +356,21 @@ class MY_Model extends Pagination {
     } 
 
    
+   /**
+    * For adding relation to database table
+    * @param array $relation
+    */
     protected function addRelation($relation = array()) {
         if(sizeof($relation) > 0) {
           $this->relations[] = $relation;
         }
     } 
 
+
+    /**
+     * For adding multiple relations to database table
+     * @param array $relations
+     */
     protected function addRelations($relations = array()) {
         if(sizeof($relations) > 0) {
           foreach($relations as $relation) {
@@ -199,9 +379,15 @@ class MY_Model extends Pagination {
         }
     } 
 
-    protected function filterRelations($result) {
 
-      if(!$this->getRelation) {
+    /**
+     * This filtering will check all the settings did in model class
+     * @param  array $result
+     * @return array
+     */
+    protected function filterResults($result) {
+
+      if(!$this->getRelation && sizeof($this->hidden) == 0) {
         return $result;
       }
 
@@ -210,6 +396,9 @@ class MY_Model extends Pagination {
       }
 
       if(sizeof($result) > 0) {
+        if(sizeof($this->hidden) > 0) {
+          $result = $this->hideData($result);
+        }
         if(sizeof($this->relations) > 0) {
           $result = $this->processRelations($result);
         }
@@ -218,6 +407,50 @@ class MY_Model extends Pagination {
       return $result;
     }
 
+
+    /**
+     * Hide result data with column names against setting did in model class
+     * @param  array $result
+     * @return array
+     */
+    protected function hideData($result) {
+
+      $type = '';
+
+      foreach($result as $res) {
+
+        if($type == '') {
+          if(is_object($res)) {
+            $type = 'object';
+          }
+          else if(is_array($res)) {
+            $type = 'array';
+          }
+        }
+
+        foreach($this->hidden as $hide) {
+          if($type == 'object') {
+            if(isset($res->$hide)) {
+              unset($res->$hide);
+            }
+          }
+          else if($type == 'array') {
+           if(isset($res[$hide])) {
+              unset($res[$hide]);
+            }  
+          }
+        }
+
+      }
+      return $result;
+    }
+
+
+    /**
+     * Process and find relations to the result data
+     * @param  array $result
+     * @return array
+     */
     protected function processRelations($result) {
         if(sizeof($this->relations) > 0) {
           foreach($this->relations as $relation) {
@@ -241,6 +474,19 @@ class MY_Model extends Pagination {
         return $result;
     }
 
+
+
+    /**
+     * Function will add relational data to result data
+     * @param  array  $result
+     * @param  string $primary
+     * @param  string $foriegn
+     * @param  string $variable
+     * @param  string $table
+     * @param  string $type
+     * @param  string $column
+     * @return array
+     */
     protected function attachRelation($result, $primary, $foriegn, $variable, $table, $type = 'table', $column = '') {
 
         $IDs = $this->getResultIDsArray($result, $primary, $variable);
@@ -275,10 +521,17 @@ class MY_Model extends Pagination {
         }
 
         return $result;
-        
     }
 
 
+    /**
+     * Find and get related rows from other tables
+     * @param  array or object $rows
+     * @param  integer $primaryID
+     * @param  string $foriegn
+     * @param  string $column
+     * @return array or object
+     */
     protected function extractRelatedRows($rows, $primaryID, $foriegn, $column = '') {
 
       $result = new stdClass();
@@ -289,7 +542,7 @@ class MY_Model extends Pagination {
         $result = '';
       }
 
-      if(sizeof($rows)) {
+      if(sizeof($rows) > 0) {
         $i = 0;
         foreach($rows as $row) {
           if($this->resultType == 'object') {
@@ -319,6 +572,12 @@ class MY_Model extends Pagination {
     }
 
 
+    /**
+     * Get the column value based on type
+     * @param  object $row    
+     * @param  string $column 
+     * @return string $column 
+     */
     protected function getColumnValue($row, $column) {
 
         $value = '';
